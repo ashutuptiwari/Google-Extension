78b7751e40b662820f67e97a75d560146ea1fc2c
ashutuptiwari <ashutosh.tiwari.cd.ece22@itbhu.ac.in>
2023-10-06 15:35:17 +0530

    integration of google calendar API with the Extracted and scraped data

--- popup.js
+++ popup.js
@@ -1,27 +1,59 @@
-let button = document.getElementById("button");
 let content1 = document.getElementById("scraped-content1");
 let content2 = document.getElementById("scraped-content2");
 let content3 = document.getElementById("analysis-result");
-
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.html) {
-    content1.textContent = request.html;
+  if (request.title) {
+    content1.textContent = request.title;
   }
-  if (request.text) {
-    content2.textContent = request.text;
+  if (request.body) {
+    content2.textContent = request.body;
   }
-  if (request.date||request.time||request.duration) {
-    const time=request.time;
-    const date=request.date;
-    const duration =request.duration;
-    const timezone= request.timezone;
-    content3.textContent = `DATE:${date} TIME:${time}  DURATION:${duration} TIMEZONE:${timezone}`;
+  if (request.date || request.time || request.duration) {
+    const time = request.time;
+    const date = request.date;
+    const duration = request.duration;
+    const timezone = request.timezone;
+    const summary=request.summary;
+    content3.textContent = `DATE:${date} TIME:${time}  DURATION:${duration} TIMEZONE:${timezone} SUMMARY:${summary}`;
+    //console.log(`DATE:${date} TIME:${time}  DURATION:${duration} TIMEZONE:${timezone}`);
+    var parts = date.split("/");
+    var day = parseInt(parts[0]);
+    var month = parseInt(parts[1]) - 1;
+    var year = parseInt(parts[2]);
+    var parts2 = time.split(":");
+    var hour = parseInt(parts2[0]);
+    var mins = parseInt(parts2[1]);
+    console.log(
+      year + ":" + month + ":" + day + ":" + hour + ":" + mins + ":" + timezone
+    +":"+summary);
+    console.log(typeof(summary));
+    const timezoneOffsets = {
+      IST: 330,
+      UTC: 0,
+      GMT: 0,
+    };
+
+    var offsetMinutes = timezoneOffsets[timezone] || 0;
+    var startDate = new Date(
+      Date.UTC(year, month, day, hour, mins) - offsetMinutes * 60 * 1000
+    );
+    var endDate=new Date(
+      Date.UTC(year, month, day, hour, mins) - (offsetMinutes-duration) * 60 * 1000
+    );
+    var formattedDate1=startDate.toISOString();
+    var formattedDate2=endDate.toISOString();
+    chrome.storage.local.set({
+      startTime:formattedDate1,
+      endTime: formattedDate2,
+      summary:summary
+    });
+    console.log(`start date: ${formattedDate1} \n end date: ${formattedDate2}`);
   } else {
-    content3.textContent = "Pata nhi kya hoagaya bc";
+    content3.textContent = "Pata nhi kya ho gaya bc";
   }
 });
 
-button.addEventListener("click", async () => {
+document.getElementById("button").addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   chrome.scripting.executeScript({
     target: {
@@ -37,17 +69,17 @@ async function scrapeEmailContent() {
   let message = {};
 
   if (h2Element && paragraph) {
-    message.html = h2Element.textContent;
-    message.text = paragraph.textContent;
+    message.title = h2Element.textContent;
+    message.body = paragraph.textContent;
   } else if (h2Element) {
-    message.html = h2Element.textContent;
-    message.text = "Content not found";
+    message.title = h2Element.textContent;
+    message.body = "Content not found";
   } else if (paragraph) {
-    message.text = paragraph.textContent;
-    message.html = "Subject not found";
+    message.title = paragraph.textContent;
+    message.body = "Subject not found";
   } else {
-    message.html = "Subject not found";
-    message.text = "Content not found";
+    message.title = "Subject not found";
+    message.bodyody = "Content not found";
   }
   const text = paragraph.textContent;
   const url = "https://ai-textraction.p.rapidapi.com/textraction";
@@ -55,32 +87,39 @@ async function scrapeEmailContent() {
     method: "POST",
     headers: {
       "content-type": "application/json",
-      "X-RapidAPI-Key": "122e1b8b9cmshee53e94722d9c72p164196jsnecec0fe800bb",
-      "X-RapidAPI-Host": "ai-textraction.p.rapidapi.com",
+      "X-RapidAPI-Key": "0196672a85msh3b26b7e915c4ddbp1a531cjsn7d2cc92dd1cd",
+      //"X-RapidAPI-Host": "ai-textraction.p.rapidapi.com",
     },
     body: JSON.stringify({
       text: text,
       entities: [
         {
-          description: "any date in the text and convert them dd/mm/yyyy format and always take year to be 2023 unless otherwise mentioned in the text",
+          description:
+            "any date in the text and convert them dd/mm/yyyy format and always take year to be 2023 unless otherwise mentioned in the text",
           type: "string",
           var_name: "date",
         },
         {
-          description: "any duration in the text in minutes or hours or days",
+          description: "any duration in the text in minutes only format",
           type: "string",
           var_name: "duration",
         },
         {
-          description: "any time mentioned in the text in HHMM format",
+          description: "any time mentioned in the text in HH:MM:SSSS format",
           type: "string",
           var_name: "time",
         },
         {
-          description: "TIMEZONE CODE OF THREE LETTERSfor example GMT,UTC, if nothing mentioned use IST",
+          description:
+            "TIMEZONE CODE OF THREE LETTERSfor example GMT,UTC, if nothing mentioned use IST",
           type: "string",
           var_name: "timezone",
-        }
+        },
+        {
+          description:"summary of the event in less than 6 words",
+          type:"string",
+          var_name:"summary",
+        },
       ],
     }),
   };
@@ -88,10 +127,15 @@ async function scrapeEmailContent() {
   try {
     const response = await fetch(url, options);
     const result = await response.json();
-    message.date = result.results.date;
-    message.time =  result.results.time;
-    message.duration =  result.results.duration;
-    message.timezone=result.results.timezone;
+    const date=result.results.date;
+    const time=result.results.time;
+    const timezone=result.results.timezone;
+    const summary = result.results.summary;
+    message.date = date;
+    message.time = time;
+    message.duration = result.results.duration||60;
+    message.timezone = timezone;
+    message.summary=summary;
   } catch (error) {
     console.error(error);
     message.date = error;
@@ -99,3 +143,58 @@ async function scrapeEmailContent() {
 
   chrome.runtime.sendMessage(message);
 }
+
+//----------------------------------------------------------------
+window.onload = function () {
+  document.getElementById("button3").addEventListener("click", function () {
+    chrome.storage.local.get(['startTime', 'endTime','summary'], function(data) {
+      const { startTime, endTime, summary } = data;
+  
+      if (startTime !== undefined && endTime !== undefined) {
+        console.log(`BUTTON3: START-TIME: ${startTime} \n END-TIME: ${endTime} SUMMARY: ${summary}`);
+        
+        chrome.identity.getAuthToken({ interactive: true }, function (token) {
+          let init = {
+            method: "POST",
+            async: true,
+            headers: {
+              Authorization: "Bearer " + token,
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              summary: summary,
+              start: {
+                dateTime: startTime,
+              },
+              end: {
+                dateTime: endTime,
+              },
+            }),
+          };
+  
+          fetch(
+            "https://www.googleapis.com/calendar/v3/calendars/primary/events?key=AIzaSyDBqNFby0J-7ewDbhjeVaJYX3g47HB2EfI",
+            init
+          )
+            .then(function (response) {
+              if (!response.ok) {
+                throw new Error("Network response was not ok");
+              }
+              return response.json();
+            })
+            .then(function (data) {
+              console.log("Event created successfully:", data);
+              // Handle the successful response, if needed
+            })
+            .catch(function (error) {
+              console.error("Error:", error);
+              // Handle the error, e.g., display an error message to the user
+            });
+        });
+      } else {
+        console.error('Data not found in storage.');
+      }
+    });
+  });
+  
+};
